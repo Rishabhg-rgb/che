d16ea54d2755a3f9f7d8813fc86104c8e405ced6
Rishabh goswami <rishabhgoswami901@gmail.com>
2024-07-25 17:30:57 +0530

    logout done

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -119,7 +119,10 @@ const DropdownUser = () => {
               </Link>
             </li>
           </ul>
-          <button className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
+          <button onClick={()=>{
+            localStorage.clear();
+            window.location.replace("/")
+          }} className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
             <svg
               className="fill-current"
               width="22"
